92011261f88d0ba640aeb0fa55405707ba1683ad
vinaykumarcherukuru <vinay.niki@gmail.com>
2021-05-09 11:57:46 +0530

    Unit Test Cases using Just & Enzyme

--- src/Components/Board/board.test.js
+++ src/Components/Board/board.test.js
@@ -1,22 +1,18 @@
-import React from 'react' 
-import Enzyme from 'enzyme';
-import {shallow, mount} from 'enzyme'
+import React from "react";
+import { shallow, mount } from "enzyme";
+import Board from './board' 
 
-import Adapter from 'enzyme-adapter-react-16';
-import Board from './board'
-
-Enzyme.configure({ adapter: new Adapter() });
-
+describe("Board", () => {
 it('renders without crashing', () => {
   let squares = Array(9).fill(null)
   shallow(<Board squares={squares}/>);
 });
 
-// it('calls onClick event on click of a board square', () =>{
-//   let squares = Array(9).fill(null) 
-//   const onClick = jest.fn();
-//   let wrapper = mount(<Board squares={squares} onClick={onClick} winLine={null} />);
-//   wrapper.find('button.squares').first().simulate('click');
-//   expect(onClick).toBeCalledWith(0)
-// })
- 
+it('calls onClick event on click of a board square', () =>{
+  let squares = Array(9).fill(null) 
+  const onClick = jest.fn();
+  let wrapper = mount(<Board squares={squares} onClick={onClick} winLine={null} />);
+  wrapper.find('button.squares').first().simulate('click');
+  expect(onClick).toBeCalledWith(0)
+})
+});
--- src/Components/Game/game.test.js
+++ src/Components/Game/game.test.js
@@ -1,37 +1,40 @@
-import React from 'react' 
-import Enzyme from 'enzyme';
-import {shallow, mount} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16';
-import Game from './game' 
+import React from "react";
+import { shallow, mount } from "enzyme";
+import Game from './game'
 
-Enzyme.configure({ adapter: new Adapter() });
+describe("Game", () => {
+  it('renders without crashing', () => {
+    shallow(<Game />);
+  });
 
-it('renders without crashing', () => {
-  shallow(<Game />);
+  it('renders game status correctly', () => {
+    const wrapper = mount(<Game />) 
+
+    // First Player   
+    const firstPlayer = wrapper.find('h3.player-info').text()
+    expect(firstPlayer).toEqual('Next player: X')
+
+    const button = wrapper.find('button.squares').first()
+    button.simulate('click')
+
+    // Second Player
+    const secondPlayer = wrapper.find('h3.player-info').text()
+    expect(secondPlayer).toEqual('Next player: Y')
+
+    //player 2
+    const turn2 = wrapper.find('button.squares').at(1)
+    turn2.simulate('click')
+    //player 1
+    const turn3 = wrapper.find('button.squares').at(4)
+    turn3.simulate('click')
+    //player 2
+    const turn4 = wrapper.find('button.squares').at(5)
+    turn4.simulate('click')
+    //player 1
+    const turn5 = wrapper.find('button.squares').at(8)
+    turn5.simulate('click')
+
+    const winner = wrapper.find('div.game-info').children().first().text()
+    expect(winner).toEqual('Game Over! Winner is: X')
+  });
 });
-
-it('renders game status correctly', () => {
-  const wrapper = mount(<Game/>) 
-  const firstPlayer = wrapper.find('player-info').text()
-  expect(firstPlayer).toEqual('Next player: X')
-
-  const button = wrapper.find('button.squares').first()
-  button.simulate('click')
-  const secondPlayer = wrapper.find('player-info').text()
-  expect(secondPlayer).toEqual('Next player: Y')
-
-  //player 2
-  const turn2 = wrapper.find('button.squares').at(1)
-  turn2.simulate('click')
-  //player 1
-  const turn3 = wrapper.find('button.squares').at(4)
-  turn3.simulate('click')
-  //player 2
-  const turn4 = wrapper.find('button.squares').at(5)
-  turn4.simulate('click')
-  //player 1
-  const turn5 = wrapper.find('button.squares').at(8)
-  turn5.simulate('click')
-  const winner = wrapper.find('div.game-info').children().first().text()
-  expect(winner).toEqual('Game Over! Winner is: X')
-})
